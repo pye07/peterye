df4c6f2433ee8be39a7e8c2abc04ca036a3a75ee
Peter Ye <pye07@ucla.edu>
2020-07-19 14:35:03 -0700

    changes

---
+++ midden/name.js
@@ -0,0 +1,19 @@
+function checkPassword(){
+  var password = document.getElementById('waiting');
+  window.name = password.value;
+    if (window.name == "" || null) {
+        return false
+    } else {
+        document.getElementById('love').innerHTML = "hi " + window.name + ", is that who you are?"
+        setTimeout(msg,2000)
+        return false
+    }
+}
+
+function msg() {
+    document.getElementById("love2").innerHTML = "I'll remember that."
+    setTimeout(dining,1000)
+      function dining() {
+        window.location.assign("/house/dining-room.html")
+      }
+}
