f0e59c08ddb93e89a6dc7d313cb7d2442c800a3a
Peter Ye <pye07@ucla.edu>
2021-03-09 13:14:54 -0800

    mute

--- peter/home_script.js
+++ peter/home_script.js
@@ -1,9 +1,18 @@
 window.onload = function() {
-  var a = document.getElementById("mutebutton")
+    var a = document.getElementById("mutebutton")
 
-a.onclick = function() {
-  document.getElementById("vids").removeAttribute("muted")
-  document.getElementById("mutepic").setAttribute("src","https://cdn2.iconfinder.com/data/icons/media-controls-5/100/vol_up-512.png")
-}
+    a.onclick = function() {
+
+        var att = document.createAttribute("muted")
+
+        if (document.getElementById("vids").hasAttribute("muted") == true) {
+            document.getElementById("mutepic").setAttribute("src", "https://cdn2.iconfinder.com/data/icons/media-controls-5/100/vol_up-512.png")
+            document.getElementById("vids").removeAttribute("muted")
+        }
+        else {
+            document.getElementById("mutepic").setAttribute("src", "https://cdn2.iconfinder.com/data/icons/media-controls-5/100/mute-512.png")
+            document.getElementById("vids").setAttributeNode(att)
+        }
+    }
 
 }
